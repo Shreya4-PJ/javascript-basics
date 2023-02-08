a749bc8edc81671b791e9b8543ffb6c28da7ab07
Shreya <shreyapj.04@gmail.com>
2023-02-08 18:04:00 +0530

    Initial Commit

---
+++ task.js
@@ -0,0 +1,55 @@
+function sound1()
+{
+    var sound=new Audio("music1.mp3");
+    sound.play()
+
+}
+
+function sound2()
+{
+    var sound=new Audio("music2.mp3");
+    sound.play()
+
+}
+
+function sound3()
+{
+    var sound=new Audio("music3.mp3");
+    sound.play()
+
+}
+
+function sound4()
+{
+    var sound=new Audio("music4.mp3");
+    sound.play()
+
+}
+
+function sound5()
+{
+    var sound=new Audio("music5.mp3");
+    sound.play()
+
+}
+
+function sound6()
+{
+    var sound=new Audio("music6.mp3");
+    sound.play()
+
+}
+
+function sound7()
+{
+    var sound=new Audio("music7.mp3");
+    sound.play()
+
+}
+
+function sound8()
+{
+    var sound=new Audio("music8.mp3");
+    sound.play()
+
+}
